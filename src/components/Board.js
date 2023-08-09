2bb1ef5bf676aeaf5d6ef85e76f025bf8b5ecb21
kimneighbor <desginkimdh@gmail.com>
2023-08-09 14:33:07 +0900

    winner 기능 추가

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import Square from "./Square";
 import "./Board.css"
+import square from "./Square";
 
 const Board = () => {
 
@@ -8,7 +9,35 @@ const Board = () => {
     // const [getter, setter]
     const [xIsNext, setXIsNext] = useState(true);
 
-    const status = `Next players ${xIsNext ? 'X' : 'O'}`;
+    const calculateWinner = (squares) => {
+        const lines= [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ]
+
+        for (let index = 0; index < lines.length; index++) {
+            const [a, b, c] = lines[index];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
+    const winner = calculateWinner(squares);
+    let status;
+    if(winner) {
+        status = 'Winner: ' + winner;
+    }else{
+        status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+    }
+
+
     const handleClick = (i) => {
         const newSquares = squares.slice();
         newSquares[i] = xIsNext ? 'X' : 'O';
@@ -17,35 +46,34 @@ const Board = () => {
 
     }
 
-
     const renderSquare = (i) => {
         return <Square value={squares[i]}
-                       onClick={() => handleClick(i)} />
+                       onClick={() => handleClick(i)}/>
     }
 
-        return (
-            <div>
-                <div className='status'>{status}</div>
+    return (
+        <div>
+            <div className='status'>{status}</div>
 
-                <div className='board-row'>
-                    {renderSquare(0)}
-                    {renderSquare(1)}
-                    {renderSquare(2)}
-                </div>
-
-                <div className='board-row'>
-                    {renderSquare(3)}
-                    {renderSquare(4)}
-                    {renderSquare(5)}
-                </div>
-
-                <div className='board-row'>
-                    {renderSquare(6)}
-                    {renderSquare(7)}
-                    {renderSquare(8)}
-                </div>
+            <div className='board-row'>
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
             </div>
-        );
+
+            <div className='board-row'>
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+            </div>
+
+            <div className='board-row'>
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+            </div>
+        </div>
+    );
 }
 
 export default Board;
