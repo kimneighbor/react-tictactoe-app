5bee3f7e0d2db4e192865fe9d7117941e8dba66f
kimneighbor <designkimdh@gmail.com>
2023-08-13 01:36:25 +0900

    각 라인의 기능에 대한 주석 추가

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import Board from "./components/Board";
 import React, {useState} from "react";
 
 function App() {
+    // 게임 히스토리 상태 관리
     const [history, setHistory] = useState([{squares: Array(9).fill(null)}]);
+    // 현재 플레이어('X' 또는 'O') 상태 관리
     const [xIsNext, setXIsNext] = useState(true);
+    // 현재 히스토리 이동(게임 진행률) 상태 관리
     const [stepNumber, setStepNumber] = useState(0);
 
+    // 게임 보드의 승자를 계산하는 함수
     const calculateWinner = (squares) => {
-
+        // 가능한 승리 조건들
         const lines = [
             [0, 1, 2],
             [3, 4, 5],
@@ -18,7 +22,8 @@ function App() {
             [2, 5, 8],
             [0, 4, 8],
             [2, 4, 6]
-        ]
+        ];
+        // 승리 조건을 검사하여 승자 확인
         for (let index = 0; index < lines.length; index++) {
 
             const [a, b, c] = lines[index];
@@ -29,10 +34,12 @@ function App() {
         return null;
     }
 
+    // 현재 이동 상태
     const current = history[stepNumber];
+    // 현재 게임 보드에서의 승자 확인
     const winner = calculateWinner(current.squares);
 
-
+    // 게임 상태 메세지
     let status;
     if (winner) {
         status = 'Winner: ' + winner;
@@ -40,6 +47,7 @@ function App() {
         status = `Next player: ${xIsNext ? 'X' : 'O'}`;
     }
 
+    // 게임 보드 클릭 이벤트 처리 함수
     const handleClick = (i) => {
         const newHistory = history.slice(0, stepNumber + 1);
         const newCurrent = newHistory[newHistory.length - 1];
@@ -54,6 +62,7 @@ function App() {
         setStepNumber(newHistory.length);
     }
 
+    // 게임 히스토리를 기록하기 위한 이동 목록 생성
     const moves = history.map((step, move) => {
         const desc = move ?
             'Go to move #' + move :
@@ -65,19 +74,19 @@ function App() {
         )
     })
 
+    // 다른 히스토리 번호로 돌아가기 위한 함수
     const jumpTo = (step) => {
         setStepNumber(step);
         setXIsNext((step % 2) === 0);
 
     }
 
-
+    // 앱 렌더링 부분: 게임 구성 요소와 정보 표시
     return (
         <div className="game">
             <div className="game-board">
                 <Board squares={current.squares} onClick={(i) => handleClick(i)}/>
             </div>
-            {/*주석 다는 방법*/}
             <div className="game-info">
                 <div className='status'>{status}</div>
                 <ol style={{listStyle: 'none'}}>{moves}</ol>
