5393655dae238c354c7b5d2a7d298f7153723e0a
kimneighbor <designkimdh@gmail.com>
2023-08-22 11:14:44 +0900

    update css

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,13 +1,23 @@
 import React, {Component} from 'react';
 import "./Square.css";
 
-    const Square = ({ onClick, value}) => {
-        return (
-            <button className="square" onClick={onClick}>
-                {value}
-            </button>
-        )
-    }
+const Square = ({onClick, value}) => {
+    // O와 X에 대한 스타일 지정
+    const style =
+        value === "X"
+            ? {color: "black"} // X의 색상은 검은색으로 지정
+            : value === "O"
+                ? {color: "white"} // O의 색상은 하얀색으로 지정
+                : {};
+
+    return (
+        <button className="square" onClick={onClick} style={style}>
+            {value}
+        </button>
+    );
+};
+
+
 export default Square;
 
 
